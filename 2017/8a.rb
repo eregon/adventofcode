0e9141ca50b14517b7539bfd7bba880134b8eb79
Benoit Daloze <eregontp@gmail.com>
2017-12-08 12:49:30 +0100

    Simplify further Day 8
    * The variables will be nil if the Regexp does not match.

--- 2017/8a.rb
+++ 2017/8a.rb
@@ -9,13 +9,10 @@ EOS
 registers = Hash.new(0)
 
 code = input.strip.lines.map { |line|
-  if /^(?<reg>\w+) (?<dir>inc|dec) (?<by>-?\d+) if (?<r>\w+) (?<cmp>[>=<!]+) (?<n>-?\d+)$/ =~ line
-    by = by.to_i * (dir == "dec" ? -1 : 1)
-    if registers[r].send(cmp, n.to_i)
-      registers[reg] += by
-    end
-  else
-    raise line
+  /^(?<reg>\w+) (?<dir>inc|dec) (?<by>-?\d+) if (?<r>\w+) (?<cmp>[>=<!]+) (?<n>-?\d+)$/ =~ line
+  by = by.to_i * (dir == "dec" ? -1 : 1)
+  if registers[r].send(cmp, n.to_i)
+    registers[reg] += by
   end
 }
 
--- 2017/8b.rb
+++ 2017/8b.rb
@@ -10,14 +10,11 @@ registers = Hash.new(0)
 max_value = 0
 
 code = input.strip.lines.map { |line|
-  if /^(?<reg>\w+) (?<dir>inc|dec) (?<by>-?\d+) if (?<r>\w+) (?<cmp>[>=<!]+) (?<n>-?\d+)$/ =~ line
-    by = by.to_i * (dir == "dec" ? -1 : 1)
-    if registers[r].send(cmp, n.to_i)
-      new_value = (registers[reg] += by)
-      max_value = [new_value, max_value].max
-    end
-  else
-    raise line
+  /^(?<reg>\w+) (?<dir>inc|dec) (?<by>-?\d+) if (?<r>\w+) (?<cmp>[>=<!]+) (?<n>-?\d+)$/ =~ line
+  by = by.to_i * (dir == "dec" ? -1 : 1)
+  if registers[r].send(cmp, n.to_i)
+    new_value = (registers[reg] += by)
+    max_value = [new_value, max_value].max
   end
 }
 
