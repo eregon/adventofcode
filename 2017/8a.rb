a3f84298850171df18c31763975faf4247394fa2
Benoit Daloze <eregontp@gmail.com>
2017-12-08 12:43:32 +0100

    Day 8

---
+++ 2017/8a.rb
@@ -0,0 +1,23 @@
+input = File.read("8.txt")
+input2 = <<EOS
+b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10
+EOS
+
+registers = Hash.new(0)
+
+code = input.strip.lines.map { |line|
+  if /^(?<reg>\w+) (?<dir>inc|dec) (?<by>-?\d+) if (?<r>\w+) (?<cmp>[>=<!]+) (?<n>-?\d+)$/ =~ line
+    by = by.to_i
+    by = -by if dir == "dec"
+    if registers[r].send(cmp, n.to_i)
+      registers[reg] += by
+    end
+  else
+    raise line
+  end
+}
+
+p registers.values.max
