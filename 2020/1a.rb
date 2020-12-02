5cc17096449404140966f9e1ba62478a0745dccb
Benoit Daloze <eregontp@gmail.com>
2020-12-02 13:15:50 +0100

    One liners

--- 2020/1a.rb
+++ 2020/1a.rb
@@ -1,2 +1,1 @@
-entries = File.readlines('1.txt').map { Integer(_1) }
-p entries.combination(2).find { |a, b| a + b == 2020 }.reduce(:*)
+p File.readlines('1.txt').map(&:to_i).combination(2).find { |a, b| a + b == 2020 }.reduce(:*)
--- 2020/1b.rb
+++ 2020/1b.rb
@@ -1,2 +1,1 @@
-entries = File.readlines('1.txt').map { Integer(_1) }
-p entries.combination(3).find { |a, b, c| a + b + c == 2020 }.reduce(:*)
+p File.readlines('1.txt').map(&:to_i).combination(3).find { |a, b, c| a + b + c == 2020 }.reduce(:*)
