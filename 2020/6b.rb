cc0ca17dd3035ca746564e1e4fddad5f54049cd5
Benoit Daloze <eregontp@gmail.com>
2020-12-06 18:00:11 +0100

    Refactor by using #tally

--- 2020/6b.rb
+++ 2020/6b.rb
@@ -1,4 +1,4 @@
 p File.read('6.txt').split(/\n{2,}/).sum { |group|
-  persons = group.lines.map(&:chomp)
-  persons.first.chars.count { |q| persons.all? { |person| person.include?(q) } }
+  persons = group.lines.size
+  group.gsub("\n", "").chars.tally.count { |q,c| c == persons }
 }
