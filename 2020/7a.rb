47e95739d1f3348e0e5ce60ef1b17af88d3b85fe
Benoit Daloze <eregontp@gmail.com>
2020-12-07 23:52:12 +0100

    Add a recursive version for Day 7 part 1

--- 2020/7a.rb
+++ 2020/7a.rb
@@ -8,27 +8,16 @@ contain = File.readlines('7.txt', chomp: true).to_h { |line|
   when /^(?<color>#{COLOR}) bags contain no other bags\.$/
     [$~[:color], []]
   when /^(?<color>#{COLOR}) bags contain (?<contained>#{N_BAGS}(, #{N_BAGS})*)\.$/
-    [$~[:color], $~[:contained].split(', ').map { |c| c[/^#{N_BAGS}$/, 2] or raise c }.to_set]
+    [$~[:color], $~[:contained].split(', ').map { |c| c[/^#{N_BAGS}$/, 2] or raise c }]
   else
     raise line
   end
 }
 
-expand = -> original do
-  # fixed point
-  expanded = original.dup.transform_values(&:dup)
-  begin
-    changed = false
-    expanded.each_pair { |color, contained|
-      contained.to_a.each { |c|
-        expanded[c].each { |cc|
-          changed = true if contained.add?(cc)
-        }
-      }
-    }
-  end while changed
-  expanded
-end
+expand = Hash.new { |expanded, color|
+  expanded[color] = contain[color].inject(contain[color].to_set) { |all, c|
+    all.merge(expand[c])
+  }
+}
 
-transitive = expand.call(contain)
-p transitive.each_pair.count { |color, contained| contained.include?("shiny gold") }
+p contain.keys.count { |color| expand[color].include?("shiny gold") }
