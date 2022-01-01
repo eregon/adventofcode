5fe5c927c7cf94f2cf83f68012a55bffad1a069b
Benoit Daloze <eregontp@gmail.com>
2022-01-01 18:35:46 +0100

    Day 16

---
+++ 2021/16a.rb
@@ -0,0 +1,42 @@
+require_relative 'lib'
+
+bits = File.read('16.txt').chomp.chars.map { "%04b" % Integer(_1, 16) }.join.freeze
+
+index = 0
+read = -> n_bits {
+  bits[index, n_bits].tap { index += n_bits }
+}
+
+int = -> n_bits {
+  Integer(read[n_bits], 2)
+}
+
+sum = 0
+
+parse = -> {
+  version = int[3]
+  sum += version
+
+  type = int[3]
+  case type
+  when 4 # read number
+    parts = ''
+    begin
+      last = int[1]
+      parts << read[4]
+    end until last == 0
+  else
+    if int[1] == 0
+      bit_length = int[15]
+      start = index
+      parse[] while index < start + bit_length
+      raise unless index == start + bit_length
+    else
+      n_packets = int[11]
+      n_packets.times { parse[] }
+    end
+  end
+}
+
+parse[]
+p sum
---
+++ 2021/16b.rb
@@ -0,0 +1,68 @@
+require_relative 'lib'
+
+bits = File.read('16.txt').chomp.chars.map { "%04b" % Integer(_1, 16) }.join.freeze
+
+index = 0
+read = -> n_bits {
+  bits[index, n_bits].tap { index += n_bits }
+}
+
+int = -> n_bits {
+  Integer(read[n_bits], 2)
+}
+
+operations = [:+, :*, :min, :max, nil, :>, :<, :==]
+
+parse = -> {
+  version = int[3]
+  type = int[3]
+
+  case type
+  when 4 # read number
+    parts = ''
+    begin
+      last = int[1]
+      parts << read[4]
+    end until last == 0
+    parts.to_i(2)
+  else
+    if int[1] == 0
+      bit_length = int[15]
+      start = index
+      args = []
+      args << parse[] while index < start + bit_length
+      raise unless index == start + bit_length
+    else
+      n_packets = int[11]
+      args = n_packets.times.map { parse[] }
+    end
+
+    [operations.fetch(type), *args]
+  end
+}
+
+interpret = -> code {
+  case code
+  when Integer
+    code
+  else
+    operation, *args = code
+    args = args.map { interpret[_1] }
+    case operation
+    when :min, :max
+      args.send(operation)
+    else
+      # !!r != r ? r : r ? 1 : 0
+      case args.reduce(operation)
+      in Integer => i then i
+      in true then 1
+      in false then 0
+      end
+    end
+  end
+}
+
+code = parse[]
+# pp code
+p interpret[code]
+
