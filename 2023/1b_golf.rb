24700f6d26757de51e76e5efd178467d7f83f294
Benoit Daloze <eregontp@gmail.com>
2023-12-02 23:03:57 +0100

    better

--- 2023/1b_golf.rb
+++ 2023/1b_golf.rb
@@ -1,3 +1,3 @@
 a=[*?0..?9,*%w[0 one two three four five six seven eight nine]].zip([*0..9]*2).to_h
 d=[*a]*?|
-p$<.sum{_1[/(#{d})(?:.*(#{d}))?/]&&a[$1]*10+a[$2||$1]}
+p$<.sum{_1[/(#{d})(.*(#{d}))?/]&&a[$1]*10+a[$3||$1]}
