ffe531692849d0a0fd1ebd957f14cc2252728d05
Benoit Daloze <eregontp@gmail.com>
2023-12-04 18:17:03 +0100

    Improve

--- 2023/4a.rb
+++ 2023/4a.rb
@@ -1,5 +1,5 @@
 p $<.readlines.sum { |line|
   winning, got = line.split(/[:|]/)[1..].map { |series| series.split.map { Integer(_1) } }
   winning_cards = got.count { |card| winning.include?(card) }
-  winning_cards > 0 ? 2 ** (winning_cards-1) : 0
+  1 << winning_cards-1
 }
