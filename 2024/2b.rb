f9470f6b240b848ef6dd7dfe1f0c1a976e8ec317
Benoit Daloze <eregontp@gmail.com>
2024-12-02 12:46:49 +0100

    Simpler: if it passes with all levels it must pass without the first level

--- 2024/2b.rb
+++ 2024/2b.rb
@@ -1,6 +1,6 @@
 p File.readlines(*$*).map { it.split.map(&:to_i) }.count { |levels|
-  (levels.size+1).times.any? { |i|
-    l = levels.dup.tap { it.delete_at(i) }
+  levels.size.times.any? { |i|
+    l = levels[...i] + levels[(i+1)..]
     l.each_cons(2).all? { _2.between?(_1+1, _1+3) } ||
     l.each_cons(2).all? { _1.between?(_2+1, _2+3) }
   }
