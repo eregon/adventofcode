a9c64f2c01a7cfaffdb6e271c29cba4443b563dd
Benoit Daloze <eregontp@gmail.com>
2024-12-02 12:52:39 +0100

    Simpler

--- 2024/2b.rb
+++ 2024/2b.rb
@@ -1,6 +1,5 @@
 p File.readlines(*$*).map { it.split.map(&:to_i) }.count { |levels|
-  levels.size.times.any? { |i|
-    l = levels[...i] + levels[(i+1)..]
+  levels.combination(levels.size-1).any? { |l|
     l.each_cons(2).all? { _2.between?(_1+1, _1+3) } ||
     l.each_cons(2).all? { _1.between?(_2+1, _2+3) }
   }
