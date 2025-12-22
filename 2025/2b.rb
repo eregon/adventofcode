2dcdeb262bbbf6240cf9e065800e2f4b17cf4154
Benoit Daloze <eregontp@gmail.com>
2025-12-22 11:15:53 +0100

    simplify

--- 2025/2b.rb
+++ 2025/2b.rb
@@ -3,31 +3,27 @@ ranges = $<.read.strip.split(",").flat_map {
   raise if a.to_i > b.to_i
 
   if a.size == b.size
-    a.to_i..b.to_i
+    a..b
   else
     raise unless a.size + 1 == b.size
     [
-      a.to_i..('9' * a.size).to_i,
-      ('1' + '0' * a.size).to_i..b.to_i
+      a..('9' * a.size),
+      ('1' + '0' * a.size)..b
     ]
   end
 }
-silly = Set.new
 
+silly = Set.new
 ranges.each { |range|
   a, b = range.begin, range.end
-  as, bs = a.to_s, b.to_s
-  s = as.size
+  s = a.size
 
-  max = s / 2
-  (1..max).each { |stride|
+  (1..s/2).each { |stride|
     if s % stride == 0
       repeats = s / stride
-      a_first_part = as[0...stride]
-      b_first_part = bs[0...stride]
-      (a_first_part..b_first_part).each { |part|
-        n = (part * repeats).to_i
-        silly << n if range.cover? n
+      (a[0...stride]..b[0...stride]).each { |part|
+        n = part * repeats
+        silly << n.to_i if range.cover? n
       }
     end
   }
